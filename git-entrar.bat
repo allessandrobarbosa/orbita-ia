c8cf136a5eb70fdbb514f2a8e251dc37702f45ba
allesandrobarbosa <allessandro.barbosa@gmail.com>
2026-07-01 22:57:44 -0300

    Atualizacao automatica via notebook/escritorio

--- git-entrar.bat
+++ git-entrar.bat
@@ -1,10 +1,19 @@
 @echo off
 SET PATH=%PATH%;%USERPROFILE%\OneDrive - mtegovbr\Área de Trabalho\Projetos\node-v24.17.0-win-x64
 
-echo [GIT] Atualizando o projeto...
-git pull origin main
+echo [GIT] Verificando se ha modificacoes locais nao salvas...
+git add .
+git stash
+
+echo.
+echo [GIT] Atualizando o projeto trazendo o historico do GitHub...
+git pull origin main --rebase -X theirs
 echo.
 
+echo [GIT] Trazendo de volta suas configuracoes locais...
+git stash pop
+
+echo.
 echo [NPM] Verificando novas dependencias...
 call npm install
 echo.
